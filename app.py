b589dbbe4d6271249622ffb3a00dad4ddfe3843e
dancorrea86 <danielmacielcorrea@gmail.com>
2021-09-29 15:05:06 -0300

    Tkinter GUI feita

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import numpy as np
 import os
-
-# Importando arquivo CSV
-dados_pastas_nomes = pd.read_csv('./csv/folders_names.csv')
+import tkinter as tk
+from tkinter.font import Font
+from PIL import Image, ImageTk
+from tkinter.filedialog import askopenfile
 
 # Local das pastas
 caminho = 'destino_pastas'
@@ -13,11 +14,53 @@ def verificar_existencia_pastas():
     return os.listdir(caminho)
 
 # Funcao que cria as pastas
-def criar_pastas():
-    if len(verificar_existencia_pastas()) > 0:
-        print("Por favor, limpe a pasta 'destino pastas'")
-    else:
-        for i in dados_pastas_nomes["NameFolder"]:
-            os.makedirs(caminho + "/" + str(i))
+def criar_pastas(nomes_pastas):
+    for i in nomes_pastas["NameFolder"]:
+        os.makedirs(caminho + "/" + str(i))
 
-criar_pastas()
+root = tk.Tk()
+
+canvas = tk.Canvas(root, width=600, height=300)
+canvas.grid(columnspan=3, rowspan=3)
+
+#Logo
+logo = Image.open('./imagens/logo.png')
+logo = ImageTk.PhotoImage(logo)
+logo_label = tk.Label(image=logo)
+logo_label.image = logo
+logo_label.grid(column=1, row=0)
+
+#instructitons
+instructons = tk.Label(root, text="Select a PDF on your computer to extract all its text", font="Raleway")
+instructons.grid(columnspan=3, column=0, row=1)
+
+def open_file():
+    browser_text.set("loading...")
+    file = askopenfile(parent=root, mode='rb', title="Chosse a file", filetype=[("CSV Files", "*.csv")])
+    if file:
+        dados_pastas_nomes = pd.read_csv(file)
+        if len(verificar_existencia_pastas()) > 0:
+            mensagem = "Por favor, limpe a pasta 'destino pastas'"
+        else:
+            criar_pastas(dados_pastas_nomes)
+            mensagem = "Pastas criadas com sucesso"
+        
+        #text box
+        instructonsFile_text.set(mensagem)
+
+#browser_button
+browser_text = tk.StringVar()
+browser_btn = tk.Button(root, textvariable=browser_text, command=lambda:open_file(), font="Raleway", bg="#20bebe", fg="white", height=2, width=15)
+browser_text.set("Browser")
+browser_btn.grid(column=1, row=2)
+
+#browser_button
+instructonsFile_text = tk.StringVar()
+instructonsFile = tk.Label(root, textvariable=instructonsFile_text, font="Raleway")
+instructonsFile_text.set("")
+instructonsFile.grid(column=1, row=3)
+
+canvas = tk.Canvas(root, width=600, height=250)
+canvas.grid(columnspan=3)
+
+root.mainloop()
